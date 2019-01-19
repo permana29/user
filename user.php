d86bce657eb44f2d8d5261b142579c9c833493bb
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-19 19:26:20 +0700

    Update user.php

--- user.php
+++ user.php
@@ -51,7 +51,7 @@ $data= ['H84391',
 'Admipoel 8',
  //reff
 'Rifky Sadega','Togop 1233','Syahdan Ajja','Rivaldi Cyber007','Neon Skylight','Jalan Pintas','Novi Liem','Edy Bitcoin',
-'Erik Rizzal','Teguh Sunahgara','Gadis Nawang','youssef miringo','muhammad ilham','Gɾȅ Ȅnɗ',
+'Erik Rizzal','Teguh Sunahgara','Gadis Nawang','youssef miringo','muhammad ilham','Greend Malik',
  ];
 $toto = ['tes',];
 $ydnews = ['tes','per-mana',];
