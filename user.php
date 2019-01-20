e86c2be82d160eaffa6c0b93740f920fc13e0319
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-20 13:34:43 +0700

    Update user.php

--- user.php
+++ user.php
@@ -7,7 +7,36 @@ $stat_ads=true;
 $sungging = $putih.'[*] '.$ijo.'subcribe youtube Per-mana '.$putih.'[-] code: '.$ijo.'J62347';
 $ads= $putih.'[*] '.$ijo.'subcribe youtube Per-mana '.$putih.'[-] code: '.$ijo.'J62347';
 $msgtoto = "[!] Subscribe Youtube Per-mana".$t.$ijo."[•••••] sekilas info [••••••]
-
+hallo guys apa kabar
+yang masih setia menunggu update script terbaru toto
+sedikit sy jelaskan perjalanan kita
+>> toto berganti menjadi TTNews 
+>> TTNews berganti menjadi watch news
+>> watch news berganti menjadi cworld
+nah saat ini cworld masih dalam tahap beta tes
+untuk yg blm punya apk cworld bisa di download
+di sini >> https://sfile.mobi/8ZPF2b181Ut
+atau kunjungi playstore
+di sini >> http://news.masjmzs.com/index/openredpacket?unique=e1d4bac1b720a2af7c0acf3d8f81617c&d=31&lng=in
+ 
+dan disini >> 
+https://play.google.com/store/apps/details?id=com.news.newsyayd
+setelah menginstall aplikasi terbaru cworld
+silahkan login menggunakan akun favorit kalian
+dan ketika di minta pilihan
+ >> new user atau old user <<
+silahkan pilih new user untuk menghilangkan jejak kita
+setelah login berhasil masuk ke tab profil
+di kanan bawah dan pergi untuk masukan kode undangan
+silahkan memasukan kode >> A15280 <<
+skrip akan sy update setelah close beta tes
+atau setelah cworld di rillis
+jika sebelum rillis ada 200+ user
+menggunakan kode saya maka skrip akan sy buat free
+tapi jika kurang dari itu
+maka system validasi akan berlaku seperti watch earn
+demikian sekilas info$putih
+msg_by:$ijo adidoank";
 $msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
 $yd_stat = true;
 $vip1= false;
