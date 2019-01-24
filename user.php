2a23524cf1c778d503b36e4760978daba083372a
permana29 <46593659+permana29@users.noreply.github.com>
2019-01-24 20:27:58 +0700

    Update user.php

--- user.php
+++ user.php
@@ -67,7 +67,7 @@ $data= ['H84391',
 'Pemburu Dollar','Muhamad Musl','Ilhamsetya','Juanda Hartono','Oraurus 1','Ramdy Amaz','Kotim GT','Adian Rivaldo','Wans Idiot','Muhammad Zakaria',
 'Firwho','tawa gembira','Fram Ajaa','Falahul Sani','Syahrul Anam','sinchan','Yoo Jinwoo','Firaun Jr','vxcv chanel','Haidar ilyas','aldiucritt',    
 'Ijal Jale','Pallaka Pall','Rezza Ramadhan','Noe Hacked','Rezza Ramadhan','Dadan dani Supriyadi','ahmad widiya','Rohman Art',
-'alfahera2321@gmail.com','Ahmad Assaqif','NI Alfin','Osky','Alditsa Dhygta2','Fajar Hidayat',"Inzal Fikri','m.salman al farisi',
+'alfahera2321@gmail.com','Ahmad Assaqif','NI Alfin','Osky','Alditsa Dhygta2','Fajar Hidayat','Inzal Fikri','m.salman al farisi',
         ];
 $toto = ['tes',];
 $ydnews = ['tes','per-mana',];
